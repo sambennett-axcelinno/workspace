6a4d4c1327845b41b4dfdc17996df43adb362fed
samuelbennett <samuelbennett@10.192.65.41>
2019-09-03 11:22:34 -0500

    added helloworld

---
+++ p1/src/p1/helloworld.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package p1;
+
+/**
+ * @author samuelbennett
+ *
+ */
+public class helloworld {
+
+	/**
+	 * 
+	 */
+	
+	int num;
+	public helloworld(int num) {
+		this.num = num;
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		helloworld test = new helloworld(5);
+		System.out.println("Hello World");
+		System.out.println(test.num);
+
+	}
+
+}
