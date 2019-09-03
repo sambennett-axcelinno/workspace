6a489cea20a54e68fef585e03655be04d5b7a3c6
samuelbennett <samuelbennett@10.192.65.41>
2019-09-03 11:56:07 -0500

    ssh works for new eclispe files

---
+++ sshtest/src/sshtest/ssh.java
@@ -0,0 +1,14 @@
+package sshtest;
+
+public class ssh {
+
+	public ssh() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SSH");
+	}
+
+}
